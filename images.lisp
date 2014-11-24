d171fbb5380bc074887b46ecaa575f6cdacc69ff
David Vázquez <davazp@gmail.com>
2014-11-24 17:10:03 +0100

    Draft of create-image

--- images.lisp
+++ images.lisp
@@ -1,5 +1,6 @@
 (defpackage :docker/images
   (:use :common-lisp :docker/request)
+  (:import-from :yason)
   (:export #:create-image
            #:list-images
            #:inspect-image
@@ -7,10 +8,24 @@
 
 (in-package :docker/images)
 
-(defun create-image (from-image)
-  (request-json (format nil "/images/create~a"
-                        (query-string "fromImage" from-image))
-                :method :post))
+(defun create-image (from-image &key (output *standard-output*) (error *error-output*))
+  "Create an image from FROM-IMAGE."
+  (multiple-value-bind (stream headers)
+      (request (format nil "/images/create~a"
+                       (query-string "fromImage" from-image))
+               :method :post)
+    (declare (ignorable headers))
+    (handler-case
+        (loop
+           for x = (yason:parse stream :object-as :alist)
+           do (print x)
+           do (let ((message (cdr (assoc "status" x :test #'string=))))
+                (when (and message output)
+                  (write-line message output)))
+           do (let ((message (cdr (assoc "error" x :test #'string=))))
+                (when (and message error)
+                  (write-line message error))))
+      (end-of-file ()))))
 
 
 (defun list-images (&key all)
