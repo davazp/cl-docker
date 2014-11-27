290d566d885a7218f19891d98a691fad64d83613
David Vázquez <davazp@gmail.com>
2014-11-27 21:42:30 +0100

    Indent code

--- all.lisp
+++ all.lisp
@@ -1,6 +1,6 @@
 (uiop/package:define-package :docker/all
   (:nicknames :docker)
   (:use-reexport :docker/errors
-		 :docker/images
+                 :docker/images
                  :docker/containers
                  :docker/misc))
