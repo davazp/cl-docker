7905c41c8e8675f9589cfb21f478afc7832318fc
David Vazquez <davazp@debian>
2014-12-07 22:41:37 +0100

    Add function PING
    It closes #37

--- misc.lisp
+++ misc.lisp
@@ -2,7 +2,8 @@
   (:use :common-lisp :docker/request)
   (:import-from :uiop #:copy-stream-to-stream)
   (:export #:info
-           #:version))
+           #:version
+	   #:ping))
 
 (in-package :docker/misc)
 
@@ -11,3 +12,16 @@
 
 (defun version ()
   (request-json "/version"))
+
+(defun ping ()
+  (with-output-to-string (out)
+    (with-open-stream (stream (request "/_ping"))
+      (loop
+	 (multiple-value-bind (line lastp)
+	     (read-line stream nil)
+	   (cond
+	     (lastp
+	      (write-string line out)
+	      (return))
+	     (t
+	      (write-line line out))))))))
