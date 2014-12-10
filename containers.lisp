980f16c384d4c2d73b14b07c40c0f1d2a51a1ae5
David Vázquez <davazp@gmail.com>
2014-12-10 01:29:15 +0100

    Export a container
    It implements both the functions `export-container-to-stream' and
    `export-container-to-pathname'.
    
    It closes #6.

--- containers.lisp
+++ containers.lisp
@@ -1,11 +1,14 @@
 (defpackage :docker/containers
   (:use :common-lisp :docker/request)
+  (:import-from :uiop #:copy-stream-to-stream)
   (:export #:list-containers
            #:inspect-container
            #:list-container-processes
            #:inspect-container-changes
            #:remove-container
-           #:wait-container))
+           #:wait-container
+           #:export-container-to-stream
+           #:export-container-to-pathname))
 
 (in-package :docker/containers)
 
@@ -39,4 +42,18 @@
 
 
 (defun wait-container (id)
-  (request-json (format nil "/containers/~a/wait" id) :method :post)))
+  (request-json (format nil "/containers/~a/wait" id) :method :post))
+
+(defun export-container-to-stream (id stream)
+  "Export the container ID as a tar archive to STREAM."
+  (with-open-stream (tar (request (format nil "/containers/~a/export" id)))
+    (copy-stream-to-stream tar stream :element-type '(unsigned-byte 8))))
+
+(defun export-container-to-pathname (id pathname &rest args &key &allow-other-keys)
+  "Export the container ID as a tar archive to PATHNAME. Keyword
+arguments are passed to the function OPEN."
+  (with-open-stream (out (apply #'open pathname
+                                :direction :output
+                                :element-type '(unsigned-byte 8)
+                                args))
+    (export-container-to-stream id out)))
